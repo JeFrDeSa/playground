236fabae6d656c219563efc1f931c1d7726d65f7
Jean <jfds.dev@gmail.com>
2023-11-07 23:51:08 +0000

    Review animation tutorials

--- animations/lib/flutter_animation_examples/animate0.dart
+++ animations/lib/flutter_animation_examples/animate0.dart
@@ -1,8 +1,6 @@
 import 'package:dartx/dartx.dart';
 import 'package:flutter/material.dart';
 
-enum Layout { one, two }
-
 /// Implements the Flutter animation example: animation0
 /// https://docs.flutter.dev/ui/animations/tutorial
 ///
@@ -10,8 +8,7 @@ enum Layout { one, two }
 /// calls setState(), which marks the current frame dirty and forces build() to
 /// be called again.
 class FlutterAnimationExample0 extends StatefulWidget {
-  /// Creates several separated spaces that will be animated during the
-  /// different tutorial steps (animation0 - animation5).
+  /// Creates a clickable container, which increases and decreases its size.
   const FlutterAnimationExample0({super.key});
 
   @override
@@ -21,45 +18,31 @@ class FlutterAnimationExample0 extends StatefulWidget {
 
 class _FlutterAnimationExample0State extends State<FlutterAnimationExample0>
     with SingleTickerProviderStateMixin {
-  Layout _currentLayout = Layout.one;
-
-  final _layoutPadding = 10.0;
+  bool _isExpanded = false;
 
   // region Layout 1
-  final _playBarHeightLayout1 = 0.225;
+  final _smallHeight = 200.0;
 
   // endregion
 
   // region Layout 2
-  final _playBarHeightLayout2 = 0.35;
+  final _bigHeight = 300.0;
 
   // endregion
 
-  late double _topLayoutHeight;
-  late double _bottomLayoutHeight;
-
-  late double _titleImageSize;
-  late double _titleInformationWidth;
-
-  late double _titleListHeight;
-  late double _titleListWidth;
-  late double _settingsButtonSize;
-  late double _favouriteDirectoriesListHeight;
-  late double _favouriteDirectoriesListWidth;
-  late double _playBarHeight;
-  late double _playBarWidth;
-
-  late Animation<double> playBarHeightAnimation;
-  late AnimationController controller;
+  late Animation<double> _containerAnimation;
+  late AnimationController animationController;
 
   @override
   void initState() {
     super.initState();
 
-    controller = AnimationController(duration: 500.milliseconds, vsync: this);
-    playBarHeightAnimation =
-    Tween<double>(begin: _playBarHeightLayout1, end: _playBarHeightLayout2)
-        .animate(controller)
+    animationController = AnimationController(
+      duration: 500.milliseconds,
+      vsync: this,
+    );
+    _containerAnimation = Tween<double>(begin: _smallHeight, end: _bigHeight)
+        .animate(animationController)
       ..addListener(() {
         setState(() {});
       });
@@ -67,177 +50,44 @@ class _FlutterAnimationExample0State extends State<FlutterAnimationExample0>
 
   @override
   void dispose() {
-    controller.dispose();
+    animationController.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    const hintText = "Flutter animation0 example.\n- Refreshes the widget subtree";
-    return Padding(
-      padding: const EdgeInsets.all(10.0),
-      child: LayoutBuilder(
-        builder: (context, constraints) {
-          _setupLayoutSpace1(
-            maxHeight: constraints.maxHeight,
-            maxWidth: constraints.maxWidth,
-          );
-          _setupTopLayout1(
-            maxHeight: constraints.maxHeight,
-            maxWidth: constraints.maxWidth,
-          );
-
-          return Column(
-            children: [
-              SizedBox(
-                height: _topLayoutHeight,
-                width: constraints.maxWidth,
-                child: Flex(
-                  direction: Axis.horizontal,
-                  children: [
-                    Container(
-                      height: _titleImageSize,
-                      width: _titleImageSize,
-                      color: Colors.blueGrey,
-                    ),
-                    Container(
-                      height: _topLayoutHeight,
-                      width: _titleInformationWidth,
-                      color: Colors.grey,
-                    ),
-                  ],
-                ),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        const SizedBox(height: 24),
+        Text(
+          "Flutter animation0 example.",
+          style: Theme.of(context).textTheme.bodyLarge,
+        ),
+        const SizedBox(height: 24),
+        Expanded(
+          child: Container(
+            alignment: Alignment.center,
+            child: Container(
+              width: _containerAnimation.value * 0.5,
+              height: _containerAnimation.value,
+              color: Colors.green,
+              child: InkWell(
+                child: const SizedBox(),
+                onTap: () {
+                  if (_isExpanded) {
+                    animationController.reverse();
+                    _isExpanded = false;
+                  } else {
+                    animationController.forward();
+                    _isExpanded = true;
+                  }
+                },
               ),
-              SizedBox(height: _layoutPadding),
-              SizedBox(
-                height: _bottomLayoutHeight,
-                width: constraints.maxWidth,
-                child: LayoutBuilder(builder: (context, constraints) {
-                  _setupBottomLayout1(
-                    maxHeight: constraints.maxHeight,
-                    maxWidth: constraints.maxWidth,
-                  );
-
-                  return Stack(
-                    children: [
-                      _positionedContainer(
-                        left: 0,
-                        top: 0,
-                        height: _titleListHeight,
-                        width: _titleListWidth,
-                        color: Colors.blue,
-                      ),
-                      _positionedContainer(
-                        right: 0,
-                        top: 0,
-                        height: _settingsButtonSize,
-                        width: _settingsButtonSize,
-                        child: ElevatedButton(
-                          onPressed: () {
-                            if (_currentLayout == Layout.one) {
-                              controller.forward();
-                              _currentLayout = Layout.two;
-                            } else {
-                              controller.reverse();
-                              _currentLayout = Layout.one;
-                            }
-                          },
-                          child: Icon(
-                            _currentLayout == Layout.one
-                                ? Icons.keyboard_double_arrow_up_rounded
-                                : Icons.keyboard_double_arrow_down_rounded,
-                          ),
-                        ),
-                      ),
-                      _positionedContainer(
-                        right: 0,
-                        top: _settingsButtonSize + _layoutPadding,
-                        height: _favouriteDirectoriesListHeight,
-                        width: _favouriteDirectoriesListWidth,
-                        color: Colors.green,
-                      ),
-                      _positionedContainer(
-                        left: 0,
-                        bottom: 0,
-                        height: _playBarHeight,
-                        width: _playBarWidth,
-                        color: Colors.amber,
-                        child: Text(
-                          hintText,
-                          style: Theme
-                              .of(context)
-                              .textTheme
-                              .bodyMedium,
-                        ),
-                      ),
-                    ],
-                  );
-                }),
-              ),
-            ],
-          );
-        },
-      ),
-    );
-  }
-
-  /// Setup the general height for the top and bottom layout one.
-  _setupLayoutSpace1({required double maxHeight, required double maxWidth}) {
-    _topLayoutHeight = maxHeight * 0.2;
-    _bottomLayoutHeight = maxHeight - _topLayoutHeight - _layoutPadding;
-  }
-
-  /// Setup the title image and title information space of layout one.
-  void _setupTopLayout1({
-    required double maxHeight,
-    required double maxWidth,
-  }) {
-    // The title image is square sized as big as the title layout.
-    _titleImageSize = _topLayoutHeight;
-    // The remaining title layout space is preserved for the title information.
-    _titleInformationWidth = maxWidth - _titleImageSize;
-  }
-
-  /// Setup the bottom space for the title list, play bar and settings of layout
-  /// one.
-  void _setupBottomLayout1({
-    required double maxHeight,
-    required double maxWidth,
-  }) {
-    _titleListHeight = maxHeight * 0.75;
-    _titleListWidth = maxWidth * 0.75;
-
-    _settingsButtonSize = maxWidth * 0.225;
-
-    _favouriteDirectoriesListHeight =
-        maxHeight - _settingsButtonSize - _layoutPadding;
-    _favouriteDirectoriesListWidth = maxWidth * 0.225;
-
-    _playBarHeight = maxHeight * playBarHeightAnimation.value;
-    _playBarWidth = _titleListWidth;
-  }
-
-  Widget _positionedContainer({
-    required double height,
-    required double width,
-    Color? color,
-    Widget? child,
-    double? left,
-    double? top,
-    double? right,
-    double? bottom,
-  }) {
-    return Positioned(
-      left: left,
-      top: top,
-      right: right,
-      bottom: bottom,
-      child: Container(
-        width: width,
-        height: height,
-        color: color,
-        child: child,
-      ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
--- animations/lib/flutter_animation_examples/animate1.dart
+++ animations/lib/flutter_animation_examples/animate1.dart
@@ -1,16 +1,13 @@
 import 'package:dartx/dartx.dart';
 import 'package:flutter/material.dart';
 
-enum Layout { one, two }
-
 /// Implements the Flutter animation example: animation1
 /// https://docs.flutter.dev/ui/animations/tutorial
 ///
-/// The AnimatedWidget base class allows to separate the core widget code from
-/// the animation code.
+/// The AnimatedWidget base class allows to separate the core widget logic from
+/// the animation logic.
 class FlutterAnimationExample1 extends StatefulWidget {
-  /// Creates several separated spaces that will be animated during the
-  /// different tutorial steps (animation0 - animation5).
+  /// Creates a clickable container, which increases and decreases its size.
   const FlutterAnimationExample1({super.key});
 
   @override
@@ -20,288 +17,96 @@ class FlutterAnimationExample1 extends StatefulWidget {
 
 class _FlutterAnimationExample1State extends State<FlutterAnimationExample1>
     with SingleTickerProviderStateMixin {
-  Layout _currentLayout = Layout.one;
-
-  final _layoutPadding = 10.0;
+  bool _isExpanded = false;
 
   // region Layout 1
-  final _playBarHeightLayout1 = 0.225;
+  final _smallHeight = 200.0;
 
   // endregion
 
   // region Layout 2
-  final _playBarHeightLayout2 = 0.35;
+  final _bigHeight = 300.0;
 
   // endregion
 
-  late double _topLayoutHeight;
-  late double _bottomLayoutHeight;
-
-  late double _titleImageSize;
-  late double _titleInformationWidth;
-
-  late double _titleListHeight;
-  late double _titleListWidth;
-  late double _settingsButtonSize;
-  late double _favouriteDirectoriesListHeight;
-  late double _favouriteDirectoriesListWidth;
-
-  // Obsolete, because the [playBarHeightAnimation] provides this information
-  // now. The [AnimatedSpace] extends the AnimationWidget, which requires a
-  // listenable (e.g. Animation<double>) that provides the values for the
-  // Animation.
-  // late double _playBarHeight;
-
-  late double _playBarWidth;
-
-  late Animation<double> playBarHeightAnimation;
-  late AnimationController controller;
+  late Animation<double> _containerAnimation;
+  late AnimationController animationController;
 
   @override
   void initState() {
     super.initState();
 
-    controller = AnimationController(duration: 500.milliseconds, vsync: this);
-    playBarHeightAnimation =
-        Tween<double>(begin: _playBarHeightLayout1, end: _playBarHeightLayout2)
-            .animate(controller);
+    animationController = AnimationController(
+      duration: 500.milliseconds,
+      vsync: this,
+    );
+    _containerAnimation = Tween<double>(begin: _smallHeight, end: _bigHeight)
+        .animate(animationController);
   }
 
   @override
   void dispose() {
-    controller.dispose();
+    animationController.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    const hintText = "Flutter animation1 example.\n- Refreshes only the widget";
-    return Padding(
-      padding: const EdgeInsets.all(10.0),
-      child: LayoutBuilder(
-        builder: (context, constraints) {
-          _setupLayoutSpace1(
-            maxHeight: constraints.maxHeight,
-            maxWidth: constraints.maxWidth,
-          );
-          _setupTopLayout1(
-            maxHeight: constraints.maxHeight,
-            maxWidth: constraints.maxWidth,
-          );
-
-          return Column(
-            children: [
-              SizedBox(
-                height: _topLayoutHeight,
-                width: constraints.maxWidth,
-                child: Flex(
-                  direction: Axis.horizontal,
-                  children: [
-                    Container(
-                      height: _titleImageSize,
-                      width: _titleImageSize,
-                      color: Colors.blueGrey,
-                    ),
-                    Container(
-                      height: _topLayoutHeight,
-                      width: _titleInformationWidth,
-                      color: Colors.grey,
-                    ),
-                  ],
-                ),
-              ),
-              SizedBox(height: _layoutPadding),
-              SizedBox(
-                height: _bottomLayoutHeight,
-                width: constraints.maxWidth,
-                child: LayoutBuilder(builder: (context, constraints) {
-                  _setupBottomLayout1(
-                    maxHeight: constraints.maxHeight,
-                    maxWidth: constraints.maxWidth,
-                  );
-
-                  return Stack(
-                    children: [
-                      _positionedContainer(
-                        left: 0,
-                        top: 0,
-                        height: _titleListHeight,
-                        width: _titleListWidth,
-                        color: Colors.blue,
-                      ),
-                      _positionedContainer(
-                        right: 0,
-                        top: 0,
-                        height: _settingsButtonSize,
-                        width: _settingsButtonSize,
-                        child: ElevatedButton(
-                          onPressed: () {
-                            if (_currentLayout == Layout.one) {
-                              controller.forward();
-                              _currentLayout = Layout.two;
-                            } else {
-                              controller.reverse();
-                              _currentLayout = Layout.one;
-                            }
-                          },
-                          child: Icon(
-                            _currentLayout == Layout.one
-                                ? Icons.keyboard_double_arrow_up_rounded
-                                : Icons.keyboard_double_arrow_down_rounded,
-                          ),
-                        ),
-                      ),
-                      _positionedContainer(
-                        right: 0,
-                        top: _settingsButtonSize + _layoutPadding,
-                        height: _favouriteDirectoriesListHeight,
-                        width: _favouriteDirectoriesListWidth,
-                        color: Colors.green,
-                      ),
-                      AnimatedSpace(
-                        animation: playBarHeightAnimation,
-                        height: _bottomLayoutHeight,
-                        width: _playBarWidth,
-                        child: Text(
-                          hintText,
-                          style: Theme
-                              .of(context)
-                              .textTheme
-                              .bodyMedium,
-                        ),
-                        left: 0,
-                        bottom: 0,
-                        color: Colors.amber,
-                      ),
-                    ],
-                  );
-                }),
-              ),
-            ],
-          );
-        },
-      ),
-    );
-  }
-
-  /// Setup the general height for the top and bottom layout one.
-  _setupLayoutSpace1({required double maxHeight, required double maxWidth}) {
-    _topLayoutHeight = maxHeight * 0.2;
-    _bottomLayoutHeight = maxHeight - _topLayoutHeight - _layoutPadding;
-  }
-
-  /// Setup the title image and title information space of layout one.
-  void _setupTopLayout1({
-    required double maxHeight,
-    required double maxWidth,
-  }) {
-    // The title image is square sized as big as the title layout.
-    _titleImageSize = _topLayoutHeight;
-    // The remaining title layout space is preserved for the title information.
-    _titleInformationWidth = maxWidth - _titleImageSize;
-  }
-
-  /// Setup the bottom space for the title list, play bar and settings of layout
-  /// one.
-  void _setupBottomLayout1({
-    required double maxHeight,
-    required double maxWidth,
-  }) {
-    _titleListHeight = maxHeight * 0.75;
-    _titleListWidth = maxWidth * 0.75;
-
-    _settingsButtonSize = maxWidth * 0.225;
-
-    _favouriteDirectoriesListHeight =
-        maxHeight - _settingsButtonSize - _layoutPadding;
-    _favouriteDirectoriesListWidth = maxWidth * 0.225;
-
-    _playBarWidth = _titleListWidth;
-  }
-
-  Widget _positionedContainer({
-    required double height,
-    required double width,
-    Color? color,
-    Widget? child,
-    double? left,
-    double? top,
-    double? right,
-    double? bottom,
-  }) {
-    return Positioned(
-      left: left,
-      top: top,
-      right: right,
-      bottom: bottom,
-      child: Container(
-        width: width,
-        height: height,
-        color: color,
-        child: child,
-      ),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        const SizedBox(height: 24),
+        Text(
+          "Flutter animation1 example.",
+          style: Theme.of(context).textTheme.bodyLarge,
+        ),
+        const SizedBox(height: 24),
+        Expanded(
+          child: Container(
+            alignment: Alignment.center,
+            child: ClickAbleContainer(
+              animation: _containerAnimation,
+              color: Colors.red,
+              onTap: () {
+                if (_isExpanded) {
+                  animationController.reverse();
+                  _isExpanded = false;
+                } else {
+                  animationController.forward();
+                  _isExpanded = true;
+                }
+              },
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
 
-class AnimatedSpace extends AnimatedWidget {
-  final double height;
-  final double width;
-  final Widget? child;
+class ClickAbleContainer extends AnimatedWidget {
+  final void Function()? onTap;
   final Color? color;
-  final double? left;
-  final double? top;
-  final double? right;
-  final double? bottom;
+  final Widget? child;
 
-  const AnimatedSpace({
+  const ClickAbleContainer({
     required Animation<double> animation,
-    required this.height,
-    required this.width,
-    this.child,
+    this.onTap,
     this.color,
-    this.left,
-    this.top,
-    this.right,
-    this.bottom,
+    this.child,
     super.key,
   }) : super(listenable: animation);
 
   @override
   Widget build(BuildContext context) {
     final animation = listenable as Animation<double>;
-    return _positionedContainer(
-      left: left,
-      top: top,
-      right: right,
-      bottom: bottom,
-      height: height * animation.value,
-      width: width,
+    return Container(
+      width: animation.value * 0.5,
+      height: animation.value,
       color: color,
-      child: child,
-    );
-  }
-
-  Widget _positionedContainer({
-    required double height,
-    required double width,
-    Color? color,
-    Widget? child,
-    double? left,
-    double? top,
-    double? right,
-    double? bottom,
-  }) {
-    return Positioned(
-      left: left,
-      top: top,
-      right: right,
-      bottom: bottom,
-      child: Container(
-        width: width,
-        height: height,
-        color: color,
-        child: child,
+      child: InkWell(
+        onTap: onTap,
+        child: const SizedBox(),
       ),
     );
   }
